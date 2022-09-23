e3ac094f9d4ba06581cb4c6b0b23d6210ca7158f
Tara78 <70331037+Tara78@users.noreply.github.com>
2022-09-23 10:12:22 +0200

    .
    .

--- code/src/components/AlbumName.jsx
+++ code/src/components/AlbumName.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 export const AlbumName = (props) => {
   return (
     <div className="album-name"> 
-      <h2 href={props.item.external_urls.spotify}>
+      <a href={props.item.external_urls.spotify}>
         {props.item.name} 
-      </h2>
+      </a>
       </div>
   )
 }
