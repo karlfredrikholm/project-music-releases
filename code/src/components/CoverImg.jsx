5da3e52eff1b4909706e3cbc3d19a55c020e62d0
Fredrik Holm <karlfredrikholm@gmail.com>
2022-09-23 17:45:48 +0200

    icons appearing when hovering

--- code/src/components/CoverImg.jsx
+++ code/src/components/CoverImg.jsx
@@ -1,21 +1,29 @@
 import React from 'react'
 
 export const CoverImg = (props) => {
-  
+
   return (
 
   <div className='cover-image-container'>
-        <a href={props.item.external_urls.spotify}>
-          <img className='cover-image' src={props.item.images[0].url} alt='album cover'/>
-          </a>
 
-          <div className='image-overlay'>
-            <div className='icons-container'>
-              <img className='heart-icon' src='/icons/heart.svg' alt='heart button'/>
-              <img className='play-icon' src='/icons/play.svg' alt='play button'/>
-              <img className='dots-icon' src='/icons/dots.svg' alt='dots button'/>
+      <div className='cover-image' 
+        style={{
+        backgroundImage: `url(${props.item.images[0].url})`,
+        backgroundSize: 'cover',
+        aspectRatio: 1 / 1,
+      }}
+      >
+
+            <div className='image-overlay'>
+              <div className='icons-container'>
+              <img className='heart-icon icons' src='/icons/heart.svg' alt='heart button'/>
+              <img className='play-icon icons' src='/icons/play.svg' alt='play button'/>
+              <img className='dots-icon icons' src='/icons/dots.svg' alt='dots button'/>
+              </div>
             </div>
-          </div>
+
       </div>
+
+  </div>
       )
 };
