22ccbc886debe00e03100b64ca4f409a4ea5e768
Fredrik Holm <97528034+karlfredrikholm@users.noreply.github.com>
2022-09-24 16:04:26 +0200

    Update App.jsx

--- code/src/App.jsx
+++ code/src/App.jsx
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import { Header } from './components/Header';
-import { AlbumList } from './components/AlbumList';
+import AlbumList from './components/AlbumList';
 
 
 
